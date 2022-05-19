1311ed1e8c50cdb828ec67a02447fe7868cb0f88
beyza ALICI <alicibeyza6@gmail.com>
2022-05-19 15:16:11 +0300

    Add project files.

---
+++ Diyetisyen/Controllers/AnaSayfaController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Diyetisyen.Controllers
+{
+    public class AnaSayfaController : Controller
+    {
+        public IActionResult AnaSayfa()
+        {
+            return View();
+        }
+        public IActionResult Hakkimda()
+        {
+            return View();
+        }
+
+        public IActionResult OnlineDiyet()
+        {
+            return View();
+        }
+
+        public IActionResult Hizmetlerimiz()
+        {
+            return View();
+        }
+
+        public IActionResult BkiHesapla()
+        {
+            return View();
+        }
+
+        public IActionResult KisaVeOz()
+        {
+            return View();
+        }
+    }
+
+}
